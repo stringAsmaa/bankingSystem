346660110611129bcb00f547b04045ca003783b2
Ola Morad <152977933+OlaMorad@users.noreply.github.com>
2025-12-08 00:15:13 +0300

    create tables and role seeder

---
+++ app/Models/Client.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\EmploymentStatus;
+use App\Enums\MaritalStatus;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'customer_number',
+        'employment_status',
+        'marital_status',
+    ];
+
+    protected $casts = [
+        'employment_status' => EmploymentStatus::class,
+        'marital_status' => MaritalStatus::class,
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bankAccounts()
+    {
+        return $this->hasMany(BankAccount::class, 'client_id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiTokens,HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +23,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'address',
+        'nationality',
+        'is_active',
+        'gender',
+        'birth_date',
     ];
 
     /**
@@ -43,6 +51,12 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'birth_date' => 'date',
+            'is_active' => 'boolean',
         ];
+    }
+    public function clients()
+    {
+        return $this->hasOne(Client::class);
     }
 }
